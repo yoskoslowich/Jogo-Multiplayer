97c59107cf66bb3d23ba938cd167286ba07d5fae
Yoskoslowich Fernandes <yoskos.fernandes@instruct.com.br>
2021-03-24 19:00:58 -0300

    Adiciona socket.io

--- playground/1st-release/public/keyboard-listener.js
+++ playground/1st-release/public/keyboard-listener.js
@@ -1,6 +1,11 @@
-export default function createKeyboardListener() {
+export default function createKeyboardListener(document) {
     const state = {
-        observers: []
+        observers: [],
+        playerId: null
+    }
+
+    function registerPlayerId(playerId) {
+        state.playerId = playerId
     }
 
     function subscribe(observerFunction) {
@@ -8,8 +13,6 @@ export default function createKeyboardListener() {
     }
 
     function notifyAll(command) {
-        // console.log(`Notifying ${state.observers.length} observers`)
-
         for(const observerFunction of state.observers) {
             observerFunction(command)
         }
@@ -21,13 +24,15 @@ export default function createKeyboardListener() {
         const keyPressed = event.key
 
         const command = {
-            playerId: 'player1',
+            type: 'move-player',
+            playerId: state.playerId,
             keyPressed
         }
-        game.movePlayer(command)
+        // game.movePlayer(command)
         notifyAll(command)
     }
     return {
-        subscribe
+        subscribe,
+        registerPlayerId
     }
 }
